88835edb8910e8001937ae25f06f480dd59ed831
Ashwani Kumar <ashwani.pulsating@yahoo.in>
2020-11-03 23:31:10 -0800

    iOS native code

---
+++ huddle-android-app/native/kosik/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  huddler
+//
+//  Created by Ashwani Kumar on 2020-07-17.
+//  Copyright © 2020 Ashwani Kumar. All rights reserved.
+//
+
+import UIKit
+import SharedCode
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width:300, height:21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.text = CommonKt.createApplicationScreenMessage()
+        view.addSubview(label)
+    }
+
+
+}
+
